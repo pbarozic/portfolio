eda50572380f86681d9d0452112f5bcb96310251
pbarozic <127948188+pbarozic@users.noreply.github.com>
2025-01-27 12:06:11 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,12 +5,27 @@ function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Pierre-Baptiste Rozic</title>
-        
+        <title>Site en cours de construction</title>
       </Head>
+      {/* 
       <Component {...pageProps} />
+      */}
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100vh',
+          fontSize: '1.5rem',
+          fontFamily: 'Arial, sans-serif',
+          textAlign: 'center',
+        }}
+      >
+        Site en cours de construction
+      </div>
     </>
   );
 }
 
 export default App;
+
